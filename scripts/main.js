48dd23785d4e1c5d5d19350f1f352bec3a3a6e4c
drventisette <drventisette@gmail.com>
2017-09-02 11:36:58 +0200

    Add noscript + Opera <12 compatibility

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,20 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', function() {
-  var canvas = document.getElementById('canvas');
-  var c = canvas.getContext('2d');
-  document.getElementById('loader').style.display = 'block';
-  setTimeout(function() {
-    HELPERS.loadAssets(CONFIG.assets, function() {
-      document.getElementById('loader').style.display = 'none';
-      HELPERS.createGame(canvas, c, CONFIG.levels);
-    });
-  }, 4000);
+  // Older Opera versions detections
+  if (navigator.userAgent.indexOf('Presto') !== -1) {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('game').style.display = 'none';
+    document.getElementById('mobileP').textContent = 'Désolé, le jeu Qurriqulum n\'est malheureusement pas compatible avec la version actuelle de votre navigateur';
+    document.getElementById('mobile').className = 'container well text-center';
+  } else {
+    var canvas = document.getElementById('canvas');
+    var c = canvas.getContext('2d');
+    document.getElementById('loader').style.display = 'block';
+    setTimeout(function() {
+      HELPERS.loadAssets(CONFIG.assets, function() {
+        document.getElementById('loader').style.display = 'none';
+        HELPERS.createGame(canvas, c, CONFIG.levels);
+      });
+    }, 4000);
+  }
 }, false);
