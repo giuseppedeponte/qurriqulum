aeb28660fbc5a410fae6918fc4bfd9668d5bd66b
drventisette <drventisette@gmail.com>
2017-09-03 17:51:41 +0200

    Add joystick image

--- scripts/joystick.js
+++ scripts/joystick.js
@@ -5,90 +5,100 @@ window.addEventListener('DOMContentLoaded', function() {
   if (!touchable) {
     return;
   }
-  var triggerEvent = function(name, key, element) {
-    console.log(key);
-     var e = new Event(name);
-      e.key=key;
-      e.keyCode=e.key.charCodeAt(0);
-      e.which=e.keyCode;
-      e.altKey=false;
-      e.ctrlKey=true;
-      e.shiftKey=false;
-      e.metaKey=false;
-      e.bubbles=true;
-      element.dispatchEvent(e);
-  };
-  var drawCircle = function(x, y) {
-    ctx.save();
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.arc(x, y, 50, 0, Math.PI*2);
-    ctx.fillStyle = 'crimson';
-    ctx.globalAlpha = 0.5;
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
-  };
-  var currentKey;
-  var onTouchStart = function(e) {
-    ctx.fillRect(0, 0, joystick.width, joystick.height);
-    currentKey = undefined;
-  };
-  var onTouchMove = function(e) {
-    e.preventDefault();
-    ctx.clearRect(0, 0, joystick.width, joystick.height);
-    ctx.fillRect(0, 0, joystick.width, joystick.height);
-    var h = e.changedTouches[0].clientX - joystick.offsetLeft < joystick.width / 2 ? 'Left' : 'Right';
-    var v = e.changedTouches[0].clientY - joystick.offsetTop < joystick.height / 2 ? 'Up' : 'Down';
-    var direction = h + ', ' + v;
-    var x;
-    var y;
-    switch (direction) {
-      case 'Left, Up': {
-        // left arrow
-        currentKey = 'ArrowLeft';
-        x = 0;
-        y = 0;
-        break;
+  var image = document.createElement('img');
+  image.src = './img/joystick.png';
+  image.addEventListener('load', function() {
+    ctx.drawImage(image,
+                  0, 0, 242, 242,
+                  0, 0, joystick.width, joystick.height);
+    var triggerEvent = function(name, key, element) {
+      element = element || window;
+       var e = new Event(name);
+        e.keyCode=key;
+        e.which=e.keyCode;
+        e.altKey=false;
+        e.ctrlKey=true;
+        e.shiftKey=false;
+        e.metaKey=false;
+        e.bubbles=true;
+        element.dispatchEvent(e);
+    };
+    var currentKey;
+    var onTouchStart = function(e) {
+      ctx.drawImage(image,
+                    0, 0, 242, 242,
+                    0, 0, joystick.width, joystick.height);
+      currentKey = undefined;
+    };
+    var onTouchMove = function(e) {
+      triggerEvent('keyup', 37);
+      triggerEvent('keyup', 38);
+      triggerEvent('keyup', 39);
+      triggerEvent('keyup', 40);
+      var tX = e.changedTouches[0].clientX - joystick.offsetLeft;
+      var tY = e.changedTouches[0].clientY - joystick.offsetTop;
+      ctx.clearRect(0, 0, joystick.width, joystick.height);
+      ctx.drawImage(image,
+                    0, 0, 242, 242,
+                    0, 0, joystick.width, joystick.height);
+      if ((tX > joystick.width / 3
+          && tX < 2 * joystick.width / 3)
+          || (tY > joystick.height / 3
+          && tY < 2 * joystick.height / 3)) {
+        return;
       }
-      case 'Right, Up': {
-        // up arrow
-        currentKey = 'ArrowUp';
-        x = joystick.width / 2;
-        y = 0;
-        break;
+      var h = tX < joystick.width / 2 ? 'Left' : 'Right';
+      var v = tY < joystick.height / 2 ? 'Up' : 'Down';
+      var direction = h + ', ' + v;
+      var x;
+      switch (direction) {
+        case 'Left, Up': {
+          // left arrow
+          currentKey = 37;
+          x = 242;
+          break;
+        }
+        case 'Right, Up': {
+          // up arrow
+          currentKey = 38;
+          x = 484;
+          break;
+        }
+        case 'Left, Down': {
+          // down arrow
+          currentKey = 40;
+          x = 726;
+          break;
+        }
+        case 'Right, Down': {
+          // right arrow
+          currentKey = 39;
+          x = 968;
+          break;
+        }
       }
-      case 'Left, Down': {
-        // down arrow
-        currentKey = 'ArrowDown';
-        x = 0;
-        y = joystick.height / 2;
-        break;
-      }
-      case 'Right, Down': {
-        // right arrow
-        currentKey = 'ArrowRight';
-        x = joystick.width / 2;
-        y = joystick.height / 2;
-        break;
-      }
-    }
-    ctx.save();
-    ctx.fillStyle = 'purple';
-    ctx.fillRect(x, y, joystick.width/2, joystick.height/2);
-    ctx.restore();
-    drawCircle(e.changedTouches[0].clientX - joystick.offsetLeft,
-               e.changedTouches[0].clientY - joystick.offsetTop);
-    triggerEvent('keydown', currentKey, joystick);
-  };
-  var onTouchEnd = function(e) {
-    ctx.clearRect(0, 0, joystick.width, joystick.height);
-    triggerEvent('keyup', currentKey, joystick);
-  };
-  joystick.addEventListener('touchstart', onTouchStart, false);
-  joystick.addEventListener('touchmove', onTouchMove, false);
-  joystick.addEventListener('touchend', onTouchEnd, false);
-  document.getElementById('spacebar').addEventListener('click', function() {
-    triggerEvent('keydown', ' ', window);
+      ctx.drawImage(image,
+                x, 0, 242, 242,
+                0, 0, joystick.width, joystick.height);
+      triggerEvent('keydown', currentKey);
+    };
+    var onTouchEnd = function(e) {
+      ctx.drawImage(image,
+                    0, 0, 242, 242,
+                    0, 0, joystick.width, joystick.height);
+      triggerEvent('keyup', 37);
+      triggerEvent('keyup', 38);
+      triggerEvent('keyup', 39);
+      triggerEvent('keyup', 40);
+    };
+    joystick.addEventListener('touchstart', onTouchStart, false);
+    joystick.addEventListener('touchmove', onTouchMove, false);
+    joystick.addEventListener('touchend', onTouchEnd, false);
+    document.getElementById('spacebar').addEventListener('click', function() {
+      triggerEvent('keydown', 32);
+    });
+    document.getElementById('escape').addEventListener('click', function() {
+      triggerEvent('keydown', 27);
+    });
   });
 });
